7f3415182a985b654e6c037e28783cab433c0a53
elenasosnina <tatalenakot@gmail.com>
2025-05-24 13:25:55 +0300

    Реализация для github  pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/Diplomnaya-rabota/",
   plugins: [react()],
+  esbuild:{
+    loader: 'jsx',
+  },
+  resolve:{
+    alias:{
+      './runtimeConfig': './runtimeConfig.browser'
+    },
+  },
+  optimizeDeps:{
+    esbuildOptions:{
+      loader:{
+        '.js': 'jsx',
+      },
+    },
+  },
 })
